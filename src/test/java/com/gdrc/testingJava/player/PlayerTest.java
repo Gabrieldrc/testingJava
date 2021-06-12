e40cd7bd89fb578a751b50c5ab1f5bbbea4a8856
Gabrieldrc <gabriel.drc25.2@gmail.com>
2021-06-12 15:42:15 -0300

    Se agregaron dos test a Player utilizando la libreria mockito para hacer un mock de Dice y forzar su resultado

--- src/test/java/com/gdrc/testingJava/player/PlayerTest.java
+++ src/test/java/com/gdrc/testingJava/player/PlayerTest.java
@@ -1,7 +1,33 @@
 package com.gdrc.testingJava.player;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    @Test
+    public void looses_when_dice_number_is_too_low() {
+        /*
+        * Como no podemos probar si el dado funciona porque
+        * devolvera numeros aleatorios que no podemos predecir,
+        * creamos un mock de la clase Dice para simular el
+        * comportamiento
+        * */
+        //Dice dice = new Dice(6);
+        Dice dice = Mockito.mock(Dice.class);
+        //Aca simulamos la funcion y forzamos a que cuando se ejecute
+        // la funcion, devuelva lo que queremos.
+        Mockito.when(dice.roll()).thenReturn(2);
+        Player player = new Player(dice, 3);
+        assertFalse(player.play());
+    }
 
+    @Test
+    public void wins_when_dice_number_is_bigger() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+    }
 }
